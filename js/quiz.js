0bb45fb9959c158f4caf7eb5298983b30642f0c8
Lancer <84729861+lancerkiller@users.noreply.github.com>
2022-08-01 20:20:18 +0800

    vishwa edit the quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,15 +1,16 @@
+console.log("welcome to add some  code vishwa,");
 const start_btn = document.querySelector(".start_button");
 const ques_div = document.getElementById("ques_div");
 var storage_answer = [];
 
 const questions = [
   {
-    question: "what is github?",
+    question: "what is github?1",
     choice1: "youtube",
     choice2: "Pokemon ",
     choice3: "Pikachu",
     choice4: "pull and push coding",
-    Answer: "choice4",
+    Answer: "choice",
   },
   {
     question: "what is github2?",
@@ -41,6 +42,11 @@ function verify_ans(client_ans) {
 */
 
 function display_question() {
+  questions.forEach((question) => {
+    console.log(question);
+    let btn = document.createElement("button");
+  });
+
   /*
   for (let i = 1; i <= 4; i++) {
     //ques_div.innerHTML = questions[i];
